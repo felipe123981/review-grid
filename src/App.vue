552d5061ab205db18ec306ecc645942726c3ea16
felipe123981 <filipe.timachine@gmail.com>
2023-06-12 00:06:55 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="catalog">
+    <div v-for="i in 3" :key="i" :id="i">
+      <Comment :cid="i"></Comment>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Comment from './components/CommentComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Comment
   }
 }
 </script>
 
 <style>
 #app {
+  background-color: #e4e9f7;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -25,4 +29,26 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.catalog {
+      /* From https://css.glass */
+background: rgba(255, 255, 255, 0.07);
+border-radius: 16px;
+box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+backdrop-filter: blur(1.7px);
+-webkit-backdrop-filter: blur(1.7px);
+border: 1px solid rgba(255, 255, 255, 0.25);
+overflow-y: scroll;
+height: 490px;
+padding: 2px;
+  /*
+            height: 490px;
+            overflow-y: scroll;
+            
+            border-radius: 16px;
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+            
+            padding: 5px;
+            background-color: var(--body-color);
+        */
+          }
 </style>
